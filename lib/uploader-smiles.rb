bf9797e7894d62c93d9de182fb0da74a6b3d219b
aicoov6cai5ju5ip <id85678581@protonmail.com>
2025-10-28 01:01:04 +0000

    add square_blur version to UploaderSmile for creating square images with blurred background
    Co-Authored-By: sketch <hello@sketch.dev>
    Change-ID: sf2328fd8b6967d4ak

--- lib/uploader-smiles.rb
+++ lib/uploader-smiles.rb
@@ -36,6 +36,17 @@ class UploaderSmile < CarrierWave::Uploader::Base
     end
   end
 
+  # Новая версия: квадратное изображение с размытым фоном
+  version :square_blur do
+    process :make_square_with_blur
+    def full_filename(for_file = model.images.file)
+      img       = for_file.split('.')
+      extension = img[-1]
+      name      = img[0...-1].join('.')
+      "#{name}_#{version_name}.#{extension}"
+    end
+  end
+
   ##
   # White list of extensions which are allowed to be uploaded:
   #
@@ -50,6 +61,62 @@ class UploaderSmile < CarrierWave::Uploader::Base
     #"#{model.randomstring}.#{model.image.file.extension}"
     if original_filename || (file && file.filename)
       @name ||= file.filename ? file.filename.gsub(/(.*_)/,'') : "#{Digest::MD5.hexdigest(File.dirname(current_path))}.#{file.extension}"
+    end
+  end
+
+  private
+
+  # Метод для создания квадратного изображения с размытым фоном
+  def make_square_with_blur
+    manipulate! do |img|
+      # Получаем размеры исходного изображения
+      width = img.columns
+      height = img.rows
+      
+      # Определяем большую и меньшую стороны
+      x = [width, height].max
+      y = [width, height].min
+      
+      # Проверяем, квадратное ли изображение
+      if width == height
+        puts "DEBUG UPLOADER: Изображение уже квадратное (#{width}x#{height}), обработка пропущена"
+        return img
+      end
+      
+      puts "DEBUG UPLOADER: Обработка изображения #{width}x#{height} -> #{x}x#{x}"
+      
+      # Сохраняем копию оригинального изображения для наложения
+      original_for_overlay = img.dup
+      
+      # Шаг 1: Создаем фоновое изображение
+      # Увеличиваем масштаб так, чтобы меньшая сторона стала равна x
+      scale_factor = x.to_f / y.to_f
+      
+      if width > height
+        # Пейзажная ориентация: увеличиваем по высоте
+        background_img = img.resize("#{(width * scale_factor).to_i}x#{x}!")
+      else
+        # Портретная ориентация: увеличиваем по ширине
+        background_img = img.resize("#{x}x#{(height * scale_factor).to_i}!")
+      end
+      
+      # Обрезаем до квадрата по центру
+      background_img = background_img.crop(Magick::CenterGravity, x, x)
+      
+      # Шаг 2: Применяем размытие к фону
+      background_img = background_img.blur_image(8, 3) # radius=8, sigma=3
+      
+      # Шаг 3: Накладываем оригинальное изображение по центру
+      # Вычисляем позицию для размещения по центру
+      overlay_x = (x - width) / 2
+      overlay_y = (x - height) / 2
+      
+      # Накладываем оригинал на размытый фон
+      result = background_img.composite(original_for_overlay, overlay_x, overlay_y, Magick::OverCompositeOp)
+      
+      puts "DEBUG UPLOADER: Квадратное изображение с размытым фоном создано: #{result.columns}x#{result.rows}"
+      
+      result
     end
   end
 
